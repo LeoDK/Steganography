eb10fe45a0a1d2fe006976f688eb23f749a1f323
DK <34984338+LeoDK@users.noreply.github.com>
2018-02-24 15:08:40 +0100

    Update filehider.py

--- filehider.py
+++ filehider.py
@@ -12,16 +12,16 @@ verbosePrint = lambda *a : None
 
 def measureTime(func):
 
-	def wrap(*args, **kwargs):
-		start_time = time.time()
-		ret = func(*args, **kwargs)
-		elapsed = time.time() - start_time
+    def wrap(*args, **kwargs):
+        start_time = time.time()
+        ret = func(*args, **kwargs)
+        elapsed = time.time() - start_time
 
-		print "Elapsed time : {} s".format(elapsed)
+        print "Elapsed time : {} s".format(elapsed)
 
-		return ret	
+        return ret    
 
-	return wrap
+    return wrap
 
 parser = ArgumentParser()
 
@@ -47,20 +47,20 @@ b_ft = True if args.ft!=0 else False
 b_ff = True if args.ff!=0 else False
 
 if ( (b_dt or b_df) and (b_ft or b_ff) ) or ( (b_dt or b_ft) and (b_df or b_ff) ) :
-	raise ValueError("You can only do one operation at once")
+    raise ValueError("You can only do one operation at once")
 
 if not True in (b_dt, b_df, b_ft, b_ff):
-	raise ValueError("No operation specified")
+    raise ValueError("No operation specified")
 
 if args.input == "\0" : raise ValueError("-i option is mandatory") 
 
 if args.quiet:
-	pass
+    pass
 
 if args.verbose:
-	def verbosePrint(*args):
-		for a in args:
-			print a
+    def verbosePrint(*args):
+        for a in args:
+            print a
 
 buf = args.buffer if args.buffer>=0 else DEF_BUF_LEN
 
@@ -68,32 +68,32 @@ buf = args.buffer if args.buffer>=0 else DEF_BUF_LEN
 @measureTime
 def op():
 
-	if b_dt:
-		assert args.output != "\0"
-		
-		hide( args.input, args.output, map(ord, list(args.dt)), buf )
+    if b_dt:
+        assert args.output != "\0"
+        
+        hide( args.input, args.output, map(ord, list(args.dt)), buf )
 
-	elif b_df:
-		assert args.output != "\0"
-		
-		data = list()
-		with open( args.df, 'rb' ) as f:
-			data_ = list(f.read())
-			data = map(ord, data_)
+    elif b_df:
+        assert args.output != "\0"
+        
+        data = list()
+        with open( args.df, 'rb' ) as f:
+            data_ = list(f.read())
+            data = map(ord, data_)
 
-		hide( args.input, args.output, data, buf )
+        hide( args.input, args.output, data, buf )
 
-	elif b_ft:
-		data = find( args.input, args.ft, buf )
-		print ''.join(c for c in map( chr, data ))
+    elif b_ft:
+        data = find( args.input, args.ft, buf )
+        print ''.join(c for c in map( chr, data ))
 
-	elif b_ff:
-		assert args.output != "\0"
+    elif b_ff:
+        assert args.output != "\0"
 
-		data_ = find( args.input, args.ff, buf )
+        data_ = find( args.input, args.ff, buf )
 
-		with open( args.output, 'wb' ) as f:
-			data = ''.join(c for c in map( chr, data_ ))
-			f.write( data )
+        with open( args.output, 'wb' ) as f:
+            data = ''.join(c for c in map( chr, data_ ))
+            f.write( data )
 
 op()
